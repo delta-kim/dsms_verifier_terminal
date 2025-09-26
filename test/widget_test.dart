7c470cb7b7bcaf39b9f0f14439c782dadaea21a9
haida333 <haida333@outlook.com>
2025-09-26 19:35:42 +0800

    Version 1.0.7

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,32 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dsms_verifier_terminal/main.dart';
+// import 'package:dsms_verifier_terminal/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  //   // Build our app and trigger a frame.
+  //   await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  //   // Verify that our counter starts at 0.
+  //   expect(find.text('0'), findsOneWidget);
+  //   expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  //   // Tap the '+' icon and trigger a frame.
+  //   await tester.tap(find.byIcon(Icons.add));
+  //   await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  //   // Verify that our counter has incremented.
+  //   expect(find.text('0'), findsNothing);
+  //   expect(find.text('1'), findsOneWidget);
+  // });
+  String body = """[국제발신]
+jfC8%2B1J5RG84w8wDnrVcQHbXpdx4mp888d6ZoOVnczAnBLQ91zH0oB0lcrgkfoEq""";
+  String msg_body = body
+      .replaceAll('[국제발신]', '') // 移除指定子串
+      .replaceAll(RegExp(r'\r\n?|\n'), '');
+  print(msg_body);
 }
